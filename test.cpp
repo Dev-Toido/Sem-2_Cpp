20488ea176bc07626f5868f381a4461e04e9ae64
Vivek Garai <garaivivek@gmail.com>
2026-04-30 07:25:04 +0530

    Add factorial and logging; delete question.txt
    Implement recursive factorial and add console logging in test.cpp (prints factorial(5) and writes messages to cerr and clog). Remove question.txt (exercise/specs file content deleted). Update compiled binary a.exe (built artifact changed).

--- test.cpp
+++ test.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int fact(int n){
+    if(n==1||n==0){
+        return n;
+    }
+    else{
+        return n*fact(n-1);
+    }
+}
 int main()
 {
-    if ((1.0+2.0) == 3.0 )
-    {
-        cout << "Yes";
-    }
-    else
-        cout << "No";
+    cout<<"HI there"<<fact(5)<<endl;
+    cerr<<"HI there"<<endl;
+    clog<<"HI there"<<endl;
+
     return 0;
 }
